95ac74b5b2c0d15d24183cc107008872cbae0a1f
Kun-Hsing Yu <khyu@muscari-8.local>
2016-03-18 18:37:02 -0400

    update survival R code

--- public/RCodes/elasticNetCox.R
+++ public/RCodes/elasticNetCox.R
@@ -13,9 +13,10 @@
 # param 8: lower bound of lambda
 # param 9: upper bound of lambda
 # param 10: hand-picked clinical variables (file)
+# param 11: session ID
 #
 # Kun-Hsing Yu
-# Dec. 8, 2015
+# March 18, 2016
 
 rm(list=ls())
 library(FSelector)
@@ -27,64 +28,68 @@ library(GGally)
 library(dplyr)
 #setwd("public/dropbox/analysis/StageOverall/")
 
-# sysargs<-commandArgs(trailingOnly=TRUE)
-# print (sysargs)
-# tumorType<-sysargs[1]
-# dataType<-sysargs[2]
-# partitionType<-sysargs[3]
-# if (partitionType == "random") {
-# 	partitionSeed<-sysargs[4]
-# 	trainingPercentage<-sysargs[5]
-# } else {
-# 	partitionSeed<-(-1)
-# 	selectedBatchesFile<-read.table(sysargs[4], sep="")
-# 	selectedBatches<-selectedBatchesFile[,1]
-# 	trainingPercentage<-(-1)
-# } 
-# minAlpha<-as.numeric(sysargs[6])
-# maxAlpha<-as.numeric(sysargs[7])
-# minLambda<-as.numeric(sysargs[8])
-# maxLambda<-as.numeric(sysargs[9])
-# if (sysargs[10] != "-1") {
-#   clinicalVariablesFile<-read.table(sysargs[10], sep="")
-#   clinicalVariables<-clinicalVariablesFile[,1]
-# }
+sysargs<-commandArgs(trailingOnly=TRUE)
+print (sysargs)
+tumorType<-sysargs[1]
+dataType<-sysargs[2]
+partitionType<-sysargs[3]
+if (partitionType == "random") {
+	partitionSeed<-sysargs[4]
+	trainingPercentage<-as.numeric(sysargs[5])
+} else {
+	partitionSeed<-(-1)
+	selectedBatchesFile<-read.table(paste("public/sessions/",sysargs[11],"/",sysargs[4],sep=""), sep="", colClasses="character")
+	selectedBatches<-selectedBatchesFile[,1]
+	trainingPercentage<-(-1)
+} 
 
-tumorTypesFile<-read.table("../../data/tumorTypes.txt", stringsAsFactors = F)
-tumorTypes<-tumorTypesFile[,1]
+minAlpha<-as.numeric(sysargs[6])
+maxAlpha<-as.numeric(sysargs[7])
+minLambda<-as.numeric(sysargs[8])
+maxLambda<-as.numeric(sysargs[9])
+if (sysargs[10] != "-1") {
+  clinicalVariablesFile<-read.table(sysargs[10], sep="")
+  clinicalVariables<-clinicalVariablesFile[,1]
+}
+
+#tumorTypesFile<-read.table("../../data/tumorTypes.txt", stringsAsFactors = F)
+#tumorTypes<-tumorTypesFile[,1]
 
 #for (i in 4:length(tumorTypes)) {
   
-tumorType<-tumorTypes[5]
-dataType<-"proteomics"
-partitionType<-"random"
-if (partitionType == "random") {
-  partitionSeed<-1
-  trainingPercentage<-0.7
-} else {
-  partitionSeed<-(-1)
-  selectedBatchesFile<-read.table(sysargs[4], sep="")
-  selectedBatches<-selectedBatchesFile[,1]
-  trainingPercentage<-(-1)
-} 
-minAlpha<--1
-maxAlpha<--1
-minLambda<--1
-maxLambda<--1
+#tumorType<-tumorTypes[5]
+#dataType<-"proteomics"
+#partitionType<-"random"
+#if (partitionType == "random") {
+#  partitionSeed<-1
+#  trainingPercentage<-0.7
+#} else {
+#  partitionSeed<-(-1)
+#  selectedBatchesFile<-read.table(sysargs[4], sep="")
+#  selectedBatches<-selectedBatchesFile[,1]
+#  trainingPercentage<-(-1)
+#} 
+#minAlpha<--1
+#maxAlpha<--1
+#minLambda<--1
+#maxLambda<--1
 
 print (maxAlpha)
 print (tumorType)
 #print (clinicalVariables)
+milestonesFileName<-paste("public/sessions/",sysargs[11],"/milestones.txt",sep="")
 
 # read files
-omicsFile<-read.table(paste("../../data/", tumorType, "_", dataType, ".txt", sep=""), stringsAsFactors=F, sep=",")
+omicsFile<-read.table(paste("../data/", tumorType, "_", dataType, ".txt", sep=""), stringsAsFactors=F, sep=",")
 print("Finished reading omics file")
-omicsNameFile<-read.table(paste("../../data/", tumorType, "_", dataType, "_ids.txt", sep=""), stringsAsFactors=F, sep="")
+write("Finished reading omics file",milestonesFileName)
+
+omicsNameFile<-read.table(paste("../data/", tumorType, "_", dataType, "_ids.txt", sep=""), stringsAsFactors=F, sep="")
 omics<-omicsFile[substr(omicsNameFile[,1],14,15)=="01",]
 omicsName<-omicsNameFile[substr(omicsNameFile[,1],14,15)=="01",]
 omicsID<-substr(omicsName, 1, 12)
 
-clinicalFile<-read.table(paste("../../data/nationwidechildrens.org_clinical_patient_", tumorType, ".txt", sep=""), stringsAsFactors = F, quote="", sep="\t")
+clinicalFile<-read.table(paste("../data/nationwidechildrens.org_clinical_patient_", tumorType, ".txt", sep=""), stringsAsFactors = F, quote="", sep="\t")
 clinical<-clinicalFile[4:dim(clinicalFile)[1],]
 colnames(clinical)<-clinicalFile[2,]
 clinicalID<-clinical[,2]
@@ -92,7 +97,7 @@ clinicalID<-clinical[,2]
 event<-clinical[,"vital_status"]
 survivedDays_alive<-clinical[,"days_to_last_followup"]
 survivedDays_dead<-clinical[,"days_to_death"]
-survivedDays<-ifelse(event == "alive", survivedDays_alive, survivedDays_dead)
+survivedDays<-ifelse(event == "Alive", survivedDays_alive, survivedDays_dead)
 
 IDintersection<-intersect(omicsID, clinicalID)
 omics<-omics[(omicsID %in% IDintersection),]
@@ -102,15 +107,15 @@ intersectIDs<-omicsID[(omicsID %in% IDintersection)]
 survivedDays<-survivedDays[(clinicalID %in% IDintersection)]
 event<-event[(clinicalID %in% IDintersection)]
 
-X<-omics
+#X<-omics
 
-# if (sysargs[10] == "-1") {
-#   X<-omics
-# } else {
-#   #X<-cbind(rnaSeq,clinical[,c(7,8,13,15,17,18,19,20,26,27,28,29,30,44,45,52)])
-#   clinicalVarColNums<-which(colnames(clinical) %in% clinicalVariables)
-#   X<-cbind(omics, clinical[,clinicalVarColNums])
-# }
+if (sysargs[10] == "-1") {
+  X<-omics
+} else {
+  #X<-cbind(rnaSeq,clinical[,c(7,8,13,15,17,18,19,20,26,27,28,29,30,44,45,52)])
+  clinicalVarColNums<-which(colnames(clinical) %in% clinicalVariables)
+  X<-cbind(omics, clinical[,clinicalVarColNums])
+}
 
 survivedDays<-ifelse(substr(survivedDays,1,2)=="[N", -1, survivedDays)
 survivedDays<-as.numeric(survivedDays)
@@ -124,6 +129,7 @@ Ymatrix<-Surv(survivedDays,event)
 Ymatrix[,1]<-Ymatrix[,1]+1
 
 print("Ymatrix done")
+write("Finished building survival matrix",milestonesFileName,append=T)
 
 if (partitionType == "random") {
   # set.seed(randSeed)
@@ -148,6 +154,8 @@ if (minLambda == -1) {
   elasticnet<-lapply(alphas, function(alpha){cv.glmnet(as.matrix(X[trainingSet,]), Ymatrix[trainingSet,], lambda=seq(minLambda,maxLambda,0.0001), alpha=alpha, family="cox")})
 }
 print("cv done")
+write("Finished regularization",milestonesFileName,append=T)
+
 minCVMs<-rep(0,length(alphas))
 for (i in 1:length(alphas)) {
   minCVMs[i]<-min(elasticnet[[i]]$cvm[2:length(elasticnet[[i]]$cvm)])
@@ -165,6 +173,7 @@ predTrain<-predict(cv.tr,as.matrix(X[trainingSet,]),s=lambdaFit,type='response')
 predTest<-predict(cv.tr,as.matrix(X[testSet,]),s=lambdaFit,type='response')
 
 print("prediction done")
+write("Finished prediction",milestonesFileName,append=T)
 
 # #Make predictions. Use cross validation to find optimal lambda if user did not specify lambda range.
 # if (minLambda == -1) {
@@ -207,6 +216,9 @@ plot(plotTrain.surv, lty = 2:3)
 
 errorThres<-0.5
 plotTest<-data.frame(cbind(predTest,survivedDays[testSet],event[testSet]))
+
+print(plotTest)
+
 for (i in 1:dim(plotTest)[1]){
   if (plotTest[i,1]>=(thres+0)){
     plotTest[i,1]<-"Poor"
@@ -226,12 +238,13 @@ plotTestGGsurv[,2]<-plotTestGGsurv[,2]/12
 plotTest.surv <- survfit(Surv(V2, V3) ~ X1, data = plotTestGGsurv)
 
 print("everything but ggsurv done")
+write("Finished building survival groups",milestonesFileName,append=T)
 
 # re-define ggsurv
 #source("../public/dropbox/analysis/Survival/ggsurv.R")
-source("ggsurv.R")
+source("public/RCodes/ggsurv.R")
 
-png(filename=paste("survivalOutput_", tumorType, ".png", sep=""), width=1000, height=500)
+png(filename=paste("public/sessions/",sysargs[11],"/survivalOutput_", tumorType, ".png", sep=""), width=1000, height=500)
 ggsurv(plotTest.surv) + 
   guides(linetype = F) + 
   xlab("Months") + ggtitle("Kaplan-Meier Curve") +
@@ -246,6 +259,6 @@ ggsurv(plotTest.surv) +
 dev.off()
 
 print("all done")
-  
+write("Completed!",milestonesFileName,append=T)
 #}
   
