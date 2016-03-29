3ed8d85c63c1be4f0b905fd850f6ee1376b52aaf
Kun-Hsing Yu <khyu@dn51t5q5.sunet>
2016-03-28 17:19:39 -0700

    update R code + decision trees

--- public/RCodes/elasticNetCox.R
+++ public/RCodes/elasticNetCox.R
@@ -16,7 +16,7 @@
 # param 11: session ID
 #
 # Kun-Hsing Yu
-# March 18, 2016
+# March 28, 2016
 
 rm(list=ls())
 library(FSelector)
@@ -26,6 +26,7 @@ library(glmnet)
 library(rms)
 library(GGally)
 library(dplyr)
+library(cvTools)
 #setwd("public/dropbox/analysis/StageOverall/")
 
 sysargs<-commandArgs(trailingOnly=TRUE)
@@ -33,15 +34,17 @@ print (sysargs)
 tumorType<-sysargs[1]
 dataType<-sysargs[2]
 partitionType<-sysargs[3]
-if (partitionType == "random") {
+if (partitionType == "random") { # random
 	partitionSeed<-sysargs[4]
 	trainingPercentage<-as.numeric(sysargs[5])
-} else {
-	partitionSeed<-(-1)
-	selectedBatchesFile<-read.table(paste("public/sessions/",sysargs[11],"/",sysargs[4],sep=""), sep="", colClasses="character")
-	selectedBatches<-selectedBatchesFile[,1]
-	trainingPercentage<-(-1)
-} 
+} else if (partitionType == "batch") { # batch
+  partitionSeed<-(-1)
+  selectedBatchesFile<-read.table(paste("public/sessions/",sysargs[11],"/",sysargs[4],sep=""), sep="", colClasses="character")
+  selectedBatches<-selectedBatchesFile[,1]
+  trainingPercentage<-(-1)
+} else if (partitionType == "kfold") {
+  nFolds<-as.numeric(sysargs[4])
+}
 
 minAlpha<-as.numeric(sysargs[6])
 maxAlpha<-as.numeric(sysargs[7])
@@ -131,13 +134,20 @@ Ymatrix[,1]<-Ymatrix[,1]+1
 print("Finished building survival matrix")
 write("Finished building survival matrix",milestonesFileName,append=T)
 
-if (partitionType == "random") {
+if (partitionType == "random"){ #random
   # set.seed(randSeed)
   trainingSet<-sample(length(survivedDays), floor(length(survivedDays)*trainingPercentage), replace=F)
   testSet<-which(!(1:length(survivedDays) %in% trainingSet))
-} else {
+  nFolds<-1
+} else if (partitionType == "kfold"){ # k fold
+  folds<-cvFolds(length(survivedDays), K=nFolds)
+} else if (partitionType == "loocv"){ # LOOCV
+  nFolds<-length(survivedDays)
+  folds<-cvFolds(length(survivedDays), K=length(survivedDays))
+} else { #batch
   trainingSet<-which(substr(iDs, 6, 7) %in% selectedBatches)
   testSet<-which(!(1:length(survivedDays) %in% trainingSet))
+  nFolds<-1
 }
 
 # cox
@@ -148,104 +158,127 @@ if (minAlpha == -1) {
 }
 alphas<-seq(minAlpha,maxAlpha,by=0.1)
 print("alphas done")
-if (minLambda == -1) {
-  elasticnet<-lapply(alphas, function(alpha){cv.glmnet(as.matrix(X[trainingSet,]), Ymatrix[trainingSet,], alpha=alpha, family="cox")})
-} else {
-  elasticnet<-lapply(alphas, function(alpha){cv.glmnet(as.matrix(X[trainingSet,]), Ymatrix[trainingSet,], lambda=seq(minLambda,maxLambda,0.0001), alpha=alpha, family="cox")})
-}
-print("Finished regularization")
-write("Finished regularization",milestonesFileName,append=T)
 
-minCVMs<-rep(0,length(alphas))
-for (i in 1:length(alphas)) {
-  minCVMs[i]<-min(elasticnet[[i]]$cvm[2:length(elasticnet[[i]]$cvm)])
-}
-optimalAlphaIndices<-which(minCVMs==min(minCVMs))
-optimalAlphaIndex<-optimalAlphaIndices[1]
-cv.tr<-cv.glmnet(as.matrix(X[trainingSet,]),Ymatrix[trainingSet,],family='cox',alpha=alphas[optimalAlphaIndex])
-if (cv.tr$lambda.min==cv.tr$lambda[1]){
-  lambdaFit<-cv.tr$lambda[2]
-} else {
-  lambdaFit<-cv.tr$lambda.min
-}
-predAll<-predict(cv.tr,as.matrix(X),s=lambdaFit,type='response')
-predTrain<-predict(cv.tr,as.matrix(X[trainingSet,]),s=lambdaFit,type='response')
-predTest<-predict(cv.tr,as.matrix(X[testSet,]),s=lambdaFit,type='response')
-
-print("Finished survival prediction")
-write("Finished survival prediction",milestonesFileName,append=T)
-
-# #Make predictions. Use cross validation to find optimal lambda if user did not specify lambda range.
-# if (minLambda == -1) {
-#   cv.tr<-cv.glmnet(X[trainingSet,],Ymatrix[trainingSet,],family='cox',alpha=alphas[optimalAlphaIndex],nfolds=10)
-#   predAll<-predict(cv.tr,as.matrix(X),s=cv.tr$lambda.min,type='response')
-#   predTrain<-predict(cv.tr,as.matrix(X[trainingSet,]),s=cv.tr$lambda.min,type='response')
-#   predTest<-predict(cv.tr,as.matrix(X[testSet,]),s=cv.tr$lambda.min,type='response')
-# } else {
-#   fit.tr<-glmnet(as.matrix(X[trainingSet,]),Ymatrix[trainingSet],family='cox',lambda=seq(minLambda,maxLambda,0.0001), alpha=alphas[optimalAlphaIndex])
-#   predAll<-predict(fit.tr,as.matrix(X),type='response')
-#   predTrain<-predict(fit.tr,as.matrix(X[trainingSet,]),type='response')
-#   predTest<-predict(fit.tr,as.matrix(X[testSet,]),type='response')
-# }
-
-# # Make predictions
-# predAllM<-predict(fit.tr,as.matrix(X),type='response')
-# predTrainM<-predict(fit.tr,as.matrix(X[trainingSet,]),type='response')
-# predTestM<-predict(fit.tr,as.matrix(X[testSet,]),type='response')
-# 
-# matrixIndex<-floor(median(which(fit.tr$df==numFeatures)))
-# 
-# predAll<-predAllM[,matrixIndex]
-# predTrain<-predTrainM[,matrixIndex]
-# predTest<-predTestM[,matrixIndex]
-
-
-thres<-median(predTrain)
-
-plotTrain<-data.frame(cbind(predTrain,survivedDays[trainingSet],event[trainingSet]))
-for (i in 1:dim(plotTrain)[1]){
-  if (plotTrain[i,1]>=thres){
-    plotTrain[i,1]<-"Poor"
-  } else {
-    plotTrain[i,1]<-"Good"
-  }  
-}
-plotTrain.surv <- survfit(Surv(V2, V3) ~ X1, data = plotTrain) 
-plot(plotTrain.surv, lty = 2:3) 
-
-
-errorThres<-0.5
-plotTest<-data.frame(cbind(predTest,survivedDays[testSet],event[testSet]))
-
-print(plotTest)
-
-for (i in 1:dim(plotTest)[1]){
-  if (plotTest[i,1]>=(thres+0)){
-    plotTest[i,1]<-"Poor"
-  } else if (plotTest[i,1]<(thres-0)){
-    plotTest[i,1]<-"Good"
-  } else {
-    plotTest[i,1]<-"Unknown"
+plotTest<-as.data.frame(cbind(0,survivedDays,event))
+for (fold in 1:nFolds) {
+  if (nFolds>1){ # kfold or loocv
+    trainingSet<-folds$subsets[,1][folds$which != fold]
+    testSet<-which(!(1:length(survivedDays) %in% trainingSet))
+    print(paste("Fold: ",fold,sep=""))
   }
+
+  if (minLambda == -1) {
+    elasticnet<-lapply(alphas, function(alpha){cv.glmnet(as.matrix(X[trainingSet,]), Ymatrix[trainingSet,], alpha=alpha, family="cox")})
+  } else {
+    elasticnet<-lapply(alphas, function(alpha){cv.glmnet(as.matrix(X[trainingSet,]), Ymatrix[trainingSet,], lambda=seq(minLambda,maxLambda,0.0001), alpha=alpha, family="cox")})
+  }
+  if (nFolds>1){
+    print(paste("Fold ",fold,": Finished regularization",sep=""))
+    write(paste("Fold ",fold,": Finished regularization",sep=""),milestonesFileName,append=T)
+  } else {
+    print("Finished regularization")
+    write("Finished regularization",milestonesFileName,append=T)
+  }
+
+  minCVMs<-rep(0,length(alphas))
+  for (i in 1:length(alphas)) {
+    minCVMs[i]<-min(elasticnet[[i]]$cvm[2:length(elasticnet[[i]]$cvm)])
+  }
+  optimalAlphaIndices<-which(minCVMs==min(minCVMs))
+  optimalAlphaIndex<-optimalAlphaIndices[1]
+  cv.tr<-cv.glmnet(as.matrix(X[trainingSet,]),Ymatrix[trainingSet,],family='cox',alpha=alphas[optimalAlphaIndex])
+  if (cv.tr$lambda.min==cv.tr$lambda[1]){
+    lambdaFit<-cv.tr$lambda[2]
+  } else {
+    lambdaFit<-cv.tr$lambda.min
+  }
+  predAll<-predict(cv.tr,as.matrix(X),s=lambdaFit,type='response')
+  predTrain<-predict(cv.tr,as.matrix(X[trainingSet,]),s=lambdaFit,type='response')
+  predTest<-predict(cv.tr,as.matrix(X[testSet,]),s=lambdaFit,type='response')
+
+  if (nFolds>1){
+    print(paste("Fold ",fold,": Finished survival prediction",sep=""))
+    write(paste("Fold ",fold,": Finished survival prediction",sep=""),milestonesFileName,append=T)
+  } else {
+    print("Finished survival prediction")
+    write("Finished survival prediction",milestonesFileName,append=T)
+  }
+
+  # #Make predictions. Use cross validation to find optimal lambda if user did not specify lambda range.
+  # if (minLambda == -1) {
+  #   cv.tr<-cv.glmnet(X[trainingSet,],Ymatrix[trainingSet,],family='cox',alpha=alphas[optimalAlphaIndex],nfolds=10)
+  #   predAll<-predict(cv.tr,as.matrix(X),s=cv.tr$lambda.min,type='response')
+  #   predTrain<-predict(cv.tr,as.matrix(X[trainingSet,]),s=cv.tr$lambda.min,type='response')
+  #   predTest<-predict(cv.tr,as.matrix(X[testSet,]),s=cv.tr$lambda.min,type='response')
+  # } else {
+  #   fit.tr<-glmnet(as.matrix(X[trainingSet,]),Ymatrix[trainingSet],family='cox',lambda=seq(minLambda,maxLambda,0.0001), alpha=alphas[optimalAlphaIndex])
+  #   predAll<-predict(fit.tr,as.matrix(X),type='response')
+  #   predTrain<-predict(fit.tr,as.matrix(X[trainingSet,]),type='response')
+  #   predTest<-predict(fit.tr,as.matrix(X[testSet,]),type='response')
+  # }
+
+  # # Make predictions
+  # predAllM<-predict(fit.tr,as.matrix(X),type='response')
+  # predTrainM<-predict(fit.tr,as.matrix(X[trainingSet,]),type='response')
+  # predTestM<-predict(fit.tr,as.matrix(X[testSet,]),type='response')
+  # 
+  # matrixIndex<-floor(median(which(fit.tr$df==numFeatures)))
+  # 
+  # predAll<-predAllM[,matrixIndex]
+  # predTrain<-predTrainM[,matrixIndex]
+  # predTest<-predTestM[,matrixIndex]
+
+
+  thres<-median(predTrain)
+
+  plotTrain<-as.data.frame(cbind(predTrain,survivedDays[trainingSet],event[trainingSet]))
+  for (i in 1:dim(plotTrain)[1]){
+    if (plotTrain[i,1]>=thres){
+      plotTrain[i,1]<-"Poor"
+    } else {
+      plotTrain[i,1]<-"Good"
+    }  
+  }
+  plotTrain.surv <- survfit(Surv(plotTrain[,2], plotTrain[,3]) ~ plotTrain[,1], data = plotTrain)
+  #plot(plotTrain.surv, lty = 2:3) 
+
+  plotTest[testSet,1]<-predTest
+  for (i in testSet){
+    if (plotTest[i,1]>=(thres)){
+      plotTest[i,1]<-"Poor"
+    } else if (plotTest[i,1]<(thres)){
+      plotTest[i,1]<-"Good"
+    }
+  }
+  print(plotTest)
+  #plotTest.surv <- survfit(Surv(V2, V3) ~ X1, data = plotTest)
+  #plot(plotTest.surv, lty = 2:3)
 }
-plotTest<-plotTest[plotTest[,1]!="Unknown",]
-plotTest.surv <- survfit(Surv(V2, V3) ~ X1, data = plotTest)
-plot(plotTest.surv, lty = 2:3) 
+
 
 # plot by ggsurv
-plotTestGGsurv<-plotTest
+if (nFolds>1){ # kfold, LOOCV
+  plotTestGGsurv<-plotTest  
+} else { # random, batch
+  plotTestGGsurv<-plotTest[testSet,]  
+}
 plotTestGGsurv[,2]<-plotTestGGsurv[,2]/12
-plotTest.surv <- survfit(Surv(V2, V3) ~ X1, data = plotTestGGsurv)
+plotTest.surv <- survfit(Surv(plotTestGGsurv[,2], plotTestGGsurv[,3]) ~ plotTestGGsurv[,1], data = plotTestGGsurv)
 
 
-survdiffTrain<-survdiff(Surv(survivedDays[trainingSet], event[trainingSet]) ~ plotTrain[,1], plotTrain)
-survdiffTest<-survdiff(Surv(survivedDays[testSet], event[testSet]) ~ plotTest[,1], plotTest)
+survdiffTrain<-survdiff(Surv(plotTrain[,2], plotTrain[,3]) ~ plotTrain[,1], plotTrain)
 pTrain<-1-pchisq(survdiffTrain$chisq, length(survdiffTrain$n)-1)
-pTest<-1-pchisq(survdiffTest$chisq, length(survdiffTest$n)-1)
-print(pTest)
-write(pTest,paste("public/sessions/",sysargs[11],"/pTest.txt",sep=""),append=T)
 
-print("everything but ggsurv done")
+if (length(unique(plotTestGGsurv[,1]))>1){
+  survdiffTest<-survdiff(Surv(plotTestGGsurv[,2], plotTestGGsurv[,3]) ~ plotTestGGsurv[,1], plotTestGGsurv)
+  pTest<-1-pchisq(survdiffTest$chisq, length(survdiffTest$n)-1)
+} else {
+  pTest<-"NA"
+}
+print(pTest)
+write(pTest,paste("public/sessions/",sysargs[11],"/pTest.txt",sep=""))
+
+print("Finished building survival groups")
 write("Finished building survival groups",milestonesFileName,append=T)
 
 # re-define ggsurv
@@ -257,10 +290,10 @@ ggsurv(plotTest.surv) +
   guides(linetype = F) + 
   xlab("Months") + ggtitle("Kaplan-Meier Curve") +
   theme(plot.title = element_text(size=24,face="bold"),
-        legend.text=element_text(size=24), 
-        legend.title=element_text(size=24), 
-        axis.text=element_text(size=18), 
-        axis.title=element_text(size=24,face="bold")) +
+    legend.text=element_text(size=24),
+    legend.title=element_text(size=24),
+    axis.text=element_text(size=18),
+    axis.title=element_text(size=24,face="bold")) +
   scale_colour_discrete(name = 'Survival Groups', labels=c('Good Prognostic Group', 'Poor Prognostic Group'))
 
 # Close and save the PNG file.
@@ -268,5 +301,3 @@ dev.off()
 
 print("Completed!")
 write("Completed!",milestonesFileName,append=T)
-#}
-  
